d56b0e2ad82700ff7f69b26b92c675de21625277
boxdox <vaibhavkandwal@pm.me>
2019-12-08 19:46:57 +0530

    added: working email submit, fixed: minor tweaks

--- src/js/app.js
+++ src/js/app.js
@@ -29,23 +29,53 @@ document
   .querySelector("#submit-btn")
   .addEventListener("click", function(event) {
     event.preventDefault(); // Prevent abrupt page reloads
-    // Creating a success message
-    let container = document.createElement("div");
-    container.setAttribute("class", "modal");
 
-    let success = document.createElement("div");
-    success.innerText = "Thank you for submitting!";
-    success.setAttribute("class", "modal-content");
+    // Reply Text
+    let responseMessage = "";
 
-    let okButton = document.createElement("div");
-    okButton.setAttribute("class", "modal-close-btn");
-    okButton.innerText = "Close";
+    // Getting and verifying the input
+    let email = document.getElementsByTagName("input")[0].value;
+    const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (emailRegex.test(email)) {
+      // Calling the API
+      fetch("https://pr4k.pythonanywhere.com/", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ email: email })
+      })
+        .then(res => {
+          if (res.status != 200) {
+            responseMessage = "Some error has occured, please try later.";
+          } else {
+            responseMessage = "Thank you for submitting!";
+          }
+        })
+        .then(() => {
+          // Creating a success message
+          let container = document.createElement("div");
+          container.setAttribute("class", "modal");
 
-    success.appendChild(okButton);
-    container.appendChild(success);
-    document.body.appendChild(container);
+          let success = document.createElement("div");
+          success.innerText = responseMessage;
+          success.setAttribute("class", "modal-content");
 
-    okButton.addEventListener("click", function() {
-      document.body.removeChild(container);
-    });
+          let okButton = document.createElement("div");
+          okButton.setAttribute("class", "modal-close-btn");
+          okButton.innerText = "Close";
+
+          success.appendChild(okButton);
+          container.appendChild(success);
+          document.body.appendChild(container);
+
+          okButton.addEventListener("click", function() {
+            document.body.removeChild(container);
+            document.getElementsByTagName("input")[0].value = "";
+          });
+        });
+    } else {
+      alert("Not a valid email :(");
+    }
   });
